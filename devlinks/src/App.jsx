7a7f906e8ff8a7dfe072c88158e6752ccd499cab
Brun0 <brunodev41@gmail.com>
2025-02-26 15:09:13 -0300

    Mudança foto ao mudar tema

--- devlinks/src/App.jsx
+++ devlinks/src/App.jsx
@@ -3,25 +3,30 @@ import Switch from "./Components//switch/Switch";
 import Links from "./Components/links/Link";
 import SocialLinks from "./Components/socialLinks/SocialLinks";
 import Footer from "./Components/footer/Footer";
-import Foto from "./img/ftperfil.jpg";
+import Foto from "./img/ImagemAlvin.jpg";
+import Foto2 from "./img/ftperfil.jpg";
 import { useState } from "react";
 
 import "./App.css";
 
 const App = () => {
   const [isLight, setIsLight] = useState();
+  const [fotoPerfil, setFotoPerfil] = useState(Foto);
+
   const troca = () => {
     setIsLight((previous) => !previous);
+    setFotoPerfil(isLight ? Foto : Foto2);
   };
+
   return (
     <div id="App" className={isLight && "light"}>
-      <Profile fotoPerfil={Foto}>@Brun0</Profile>
+      <Profile fotoPerfil={fotoPerfil}>@Brun0</Profile>
       <Switch troca={troca} islight={isLight} />
       <ul>
         <Links link={"https://github.com/Brun0HM"}>GitHub</Links>
         <Links link={"https://www.instagram.com/fk.brun0/"}>Instagram</Links>
-        <Links>Portifólio</Links>
-        <Links>Me pague um café</Links>
+        <Links link={"https://portfolio.com"}>Portifólio</Links>
+        <Links link={"https://nubank.com"}>Me pague um café</Links>
       </ul>
 
       <div id="socialLinks">
