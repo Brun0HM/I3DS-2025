9560af3a4f2e29691a039a661da89b728d4ec795
Brun0 <brunodev41@gmail.com>
2025-02-26 14:18:37 -0300

    Correção caixa links

--- devlinks/src/Components/switch/Switch.jsx
+++ devlinks/src/Components/switch/Switch.jsx
@@ -2,8 +2,8 @@ import style from "./Switch.module.css";
 
 const Switch = ({ troca, islight }) => {
   return (
-    <div onClick={troca} className={islight && style.light}>
-      <div id={style.switch}>
+    <div className={islight && style.light}>
+      <div onClick={troca} id={style.switch}>
         <button></button>
         <span></span>
       </div>
