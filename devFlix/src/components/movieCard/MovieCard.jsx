1e204986f67f087f8544e2e8c5a836eb588ec81e
Brun0 <brunodev41@gmail.com>
2025-03-18 14:41:48 -0300

    Ajustes MovieDescription

--- devFlix/src/components/movieCard/MovieCard.jsx
+++ devFlix/src/components/movieCard/MovieCard.jsx
@@ -1,24 +1,30 @@
 import styles from "./MovieCard.module.css";
 import MovieDescription from "../movieDescription/MovieDescription";
+import { useState } from "react";
 
-
-const MovieCard = ({apiUrl, ...props}) => {
+const MovieCard = (props) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
   return (
     <>
-    <div className={styles.movie}>
-      <div>
-        <p>{props.Year}</p>
+      <div className={styles.movie} onClick={toggleModal}>
+        <div>
+          <p>{props.Year}</p>
+        </div>
+        <div>
+          <img src={props.Poster} alt="" />
+        </div>
+        <div>
+          <span>{props.Type}</span>
+          <h3>{props.Title}</h3>
+          <p>{props.Description}</p>
+        </div>
       </div>
-      <div>
-        <img src={props.Poster} alt="" />
-      </div>
-      <div>
-        <span>{props.Type}</span>
-        <h3>{props.Title}</h3>
-        <p>{props.Description}</p>
-      </div>
-    </div>
-    <MovieDescription apiUrl={apiUrl} movieID={props.imdbID} />
+      {isModalOpen && (
+        <MovieDescription apiUrl={props.apiUrl} movieID={props.imdbID} click={toggleModal}  />
+      )}
     </>
   );
 };
